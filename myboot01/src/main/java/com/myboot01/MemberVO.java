51b443b6d577b302300e5f8d0a60d7010ca64eea
LAPTOP-FAT0OK28\lemon <lemona9201@naver.com>
2021-11-09 21:29:11 +0900

    first commit

---
+++ myboot01/src/main/java/com/myboot01/MemberVO.java
@@ -0,0 +1,14 @@
+package com.myboot01;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class MemberVO {
+	private String id;
+	private String name;
+	
+//	public String getId() {
+//		return id;
+//	}
+}
